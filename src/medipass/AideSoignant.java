363097976c3a283c69ccc7550defab7cb7a8e09a
ulysse <ulyssefassinou@gmail.com>
2025-11-22 14:36:05 +0100

    Mise à jour des classes AideSoignant et Consultation

--- src/medipass/AideSoignant.java
+++ src/medipass/AideSoignant.java
@@ -2,4 +2,51 @@ package medipass;
 
 public class AideSoignant {
 
+    private int id;
+    private String nom;
+
+    public AideSoignant(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    // Vérifier si un médecin est disponible
+    public boolean verifierDisponibiliteMedecin(Medecin medecin, String date, int heure) {
+        if (medecin.estOccupe(date, heure)) {
+            System.out.println("Le médecin " + medecin.getNom() + " n'est pas disponible à " + heure + "h le " + date + ".");
+            return false;
+        } else {
+            System.out.println("Le médecin " + medecin.getNom() + " est disponible à " + heure + "h le " + date + ".");
+            return true;
+        }
+    }
+
+    // Planifier une consultation
+    public void planifierConsultation(Consultation consultation) {
+        System.out.println("Aide-soignant " + nom + " a planifié une consultation.");
+        consultation.programmer();
+    }
+
+    // Annuler une consultation
+    public void annulerConsultation(Consultation consultation) {
+        System.out.println("Aide-soignant " + nom + " a annulé une consultation.");
+        consultation.annuler();
+    }
+
+    // Prendre les signes vitaux
+    public void enregistrerSignesVitaux(Patient p, String tension, float poids, float temperature) {
+        System.out.println("Signes vitaux du patient " + p.getNom() + " :");
+        System.out.println("- Tension : " + tension);
+        System.out.println("- Poids : " + poids + " kg");
+        System.out.println("- Température : " + temperature + "°C");
+    }
+
+    // Préparer la salle pour la consultation
+    public void preparerSalleConsultation() {
+        System.out.println("Salle de consultation préparée par l'aide-soignant " + nom+".");
+}
 }
--- src/medipass/Consultation.java
+++ src/medipass/Consultation.java
@@ -2,4 +2,27 @@ package medipass;
 
 public class Consultation {
 
+    private Medecin medecin;
+    private Patient patient;
+    private String date;
+    private int heure;
+    private boolean estProgrammee;
+
+    public Consultation(Medecin medecin, Patient patient, String date, int heure) {
+        this.medecin = medecin;
+        this.patient = patient;
+        this.date = date;
+        this.heure = heure;
+        this.estProgrammee = false;
+    }
+
+    public void programmer() {
+        estProgrammee = true;
+        System.out.println("Consultation programmée pour " + patient.getNom() + " avec " + medecin.getNom() + " à " + heure + "h le " + date + ".");
+    }
+
+    public void annuler() {
+        estProgrammee = false;
+        System.out.println("Consultation annulée pour " + patient.getNom()+".");
+}
 }
